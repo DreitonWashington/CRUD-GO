dc44656128bb2f97cbfbe62c04a498054baa42a2
DreitonWashington <dreiton.washington@gmail.com>
2023-11-30 18:47:14 +0000

    Created request and response model

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,7 +1,20 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	errorhandler "github.com/DreitonWashington/CRUD-GO/src/configuration/errorHandler"
+	"github.com/DreitonWashington/CRUD-GO/src/controller/model/request"
+	"github.com/gin-gonic/gin"
+)
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		erro := errorhandler.NewBadRequestError(fmt.Sprintf("There are some incorrect fields, error=%s", err.Error()))
+		c.JSON(erro.Code, erro)
+		return
+	}
+	fmt.Println(userRequest)
 }
