ed0c7958fbeeaf4f57deab828a30f22f7125156d
DreitonWashington <dreiton.washington@gmail.com>
2023-12-01 03:05:25 +0000

    Adding Logs

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,21 +2,24 @@ package controller
 
 import (
 	"fmt"
-	"log"
 
+	"github.com/DreitonWashington/CRUD-GO/src/configuration/logger"
 	"github.com/DreitonWashington/CRUD-GO/src/configuration/validation"
 	"github.com/DreitonWashington/CRUD-GO/src/controller/model/request"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller", zap.String("journey", "createUser"))
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("There are some incorrect fields, error=%s", err.Error())
+		logger.Error("Error trying to validate user info", err, zap.String("journey", "createUser"))
 		erro := validation.ValidateUserError(err)
 		c.JSON(erro.Code, erro)
 		return
 	}
 	fmt.Println(userRequest)
+	logger.Info("User created successfully", zap.String("journey", "createUser"))
 }
